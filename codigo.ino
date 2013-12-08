aaa15217fcb8f7a541ba33217c8448d9ceac9dc4
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2013-12-08 18:21:23 -0200

    refatorando os botoes (structs) para terem dados dos estados do botao apertado e qual o pin relacionado.

--- codigo.ino
+++ codigo.ino
@@ -20,7 +20,7 @@
 int RECV_PIN = 11;
 int FIRST_BUTTON_PIN = 12;
 //================================================================================================================================
-// int SECOND_BUTTON_PIN = 3; //@Lanchinho : nao necessariamente neste pino/ casa, pode ser qlqer outra que esteja livre
+int SECOND_BUTTON_PIN = 2;
 // int THIRD_BUTTON_PIN = 4;  //@Lanchinho: Vou deixar todas as cagadas, digo alterações feitas por mim separadas por um "========="
 //================================================================================================================================
 int STATUS_PIN = 13;
@@ -34,6 +34,9 @@ decode_results results;
 typedef struct Button_Info{
     int codeType;// -1 should be the default
     int toggle;  //0 should be the default; The RC5/6 toggle state
+    int button_pin; //the button pin of this button
+    int button_state; //the actual state of this button, if been pressed or not
+    int last_button_state; //the last state, to verify if this button has been released or not
     unsigned long codeValue; // The code value if not raw
     unsigned int rawCodes[RAWBUF]; // The durations if raw
     int codeLen; // The length of the code
@@ -44,10 +47,15 @@ typedef struct Button_Info{
 //=====================
 int last_pressed_button = 0; //por default o ultimo botao pressionado vai ser o primeiro
 
-Button_Info button_one = { -1, 0 };// inicializando codeType =-1 e toggle=0;
+// inicializando codeType =-1 e toggle=0, e o button bin como o do FIRST_BUTTON_PIN;
+//button_state = 0, e last_button_state =0 tmb, indicando que estava sem apertar e continua assim.
+Button_Info button_one = { -1, 0, FIRST_BUTTON_PIN, 0, 0 };
+// inicializando codeType =-1 e toggle=0, e o button bin como o do SECOND_BUTTON_PIN;
+//button_state = 0, e last_button_state =0 tmb, indicando que estava sem apertar e continua assim.
+Button_Info button_two = { -1, 0, SECOND_BUTTON_PIN, 0, 0 };
 
 //lista dos botoes
-Button_Info buttons_list[1] = { button_one };
+Button_Info buttons_list[2] = { button_one, button_two };
 
 //=====================
 //VARIAVEIS GLOBAIS - FIM
@@ -59,17 +67,11 @@ void setup()
     Serial.begin(9600);
     irrecv.enableIRIn(); // Start the receiver
     pinMode(FIRST_BUTTON_PIN, INPUT);
-    // pinMode(SECOND_BUTTON_PIN, INPUT);
+    pinMode(SECOND_BUTTON_PIN, INPUT);
     // pinMode(THIRD_BUTTON_PIN, INPUT);
     pinMode(STATUS_PIN, OUTPUT);
 }
 
-// Storage for the recorded code
-// int codeType = -1; // The type of code
-// unsigned long codeValue; // The code value if not raw
-// unsigned int rawCodes[RAWBUF]; // The durations if raw
-// int codeLen; // The length of the code
-// int toggle = 0; // The RC5/6 toggle state
 
 
 // Stores the code for later playback
@@ -219,15 +221,8 @@ void sendCodeUnknow(int pressed_button)
 
 
 
-int lastButtonState; //@Lanchinho, seria isto aqui o ultimo estado do btn 1 ?
 
 void printDadosBotao(int pressed_button){
-
-    // int codeType;// -1 should be the default
-    // int toggle;  //0 should be the default; The RC5/6 toggle state
-    // unsigned long codeValue; // The code value if not raw
-    // unsigned int rawCodes[RAWBUF]; // The durations if raw
-    // int codeLen; // The length of the code
 
     Serial.println("Dados Button");
     Serial.println("codeType:");
@@ -241,32 +236,45 @@ void printDadosBotao(int pressed_button){
 
 }
 //============================================================
+// int lastButtonState;
 // int lastBtnStateOfBtn2;
 // int lastBtnStateOfBtn3;
 //============================================================
 
+int check_button_released(int button_index){
+    return buttons_list[button_index].last_button_state == HIGH && buttons_list[button_index].button_state == LOW;
+}
+
+int check_button_pressed(int button_index){
+    return buttons_list[button_index].button_state;
+}
+
+
+
 void loop()
 {
     // If button pressed, send the code.
-    int firstButtonState = digitalRead(FIRST_BUTTON_PIN);
 
     //========================================================
+    buttons_list[0].button_state = digitalRead(FIRST_BUTTON_PIN);
+    buttons_list[1].button_state = digitalRead(SECOND_BUTTON_PIN);
+
     // int secondButtonState = digitalRead(SECOND_BUTTON_PIN);
     // int thirdButtonState = digitalRead(THIRD_BUTTON_PIN);
     //========================================================
 
-    if (lastButtonState == HIGH && firstButtonState == LOW)
+    if (check_button_released(0))
     {
         Serial.println("Released");
         irrecv.enableIRIn(); // Re-enable receiver
     }
 
-    if (firstButtonState)
+    if (check_button_pressed(0))
     {
         last_pressed_button = 0; //marca o primeiro botao como sendo o pressionado
         Serial.println("Pressed, sending");
         digitalWrite(STATUS_PIN, HIGH);
-        sendCode(lastButtonState == firstButtonState, last_pressed_button);
+        sendCode(buttons_list[0].last_button_state == buttons_list[0].button_state, last_pressed_button);
         digitalWrite(STATUS_PIN, LOW);
         delay(50); // Wait a bit between retransmissions
     }
@@ -278,61 +286,7 @@ void loop()
         irrecv.resume(); // resume receiver
         digitalWrite(STATUS_PIN, LOW);
     }
-    lastButtonState = firstButtonState;
-
-//==========================================================================
-//Btn2
-    // if (lastBtnStateOfBtn2 == HIGH && secondButtonState == LOW)
-    // {
-    //     Serial.println("Released");
-    //     irrecv.enableIRIn(); // Re-enable receiver
-    // }
-
-    // if (secondButtonState)
-    // {
-    //     Serial.println("Pressed, sending");
-    //     digitalWrite(STATUS_PIN, HIGH);
-    //     sendCode(lastBtnStateOfBtn2 == secondButtonState);
-    //     digitalWrite(STATUS_PIN, LOW);
-    //     delay(50); // Wait a bit between retransmissions
-    // }
-    // else if (irrecv.decode(&results))
-    // {
-    //     digitalWrite(STATUS_PIN, HIGH);
-    //     storeCode(&results);
-    //     irrecv.resume(); // resume receiver
-    //     digitalWrite(STATUS_PIN, LOW);
-    // }
-    // lastBtnStateOfBtn2 = lastBtnStateOfBtn2;
-
-    // //Btn 3
-    // if (lastBtnStateOfBtn3 == HIGH && thirdButtonState == LOW)
-    // {
-    //     Serial.println("Released");
-    //     irrecv.enableIRIn(); // Re-enable receiver
-    // }
-
-    // if (thirdButtonState)
-    // {
-    //     Serial.println("Pressed, sending");
-    //     digitalWrite(STATUS_PIN, HIGH);
-    //     sendCode(lastBtnStateOfBtn3 == thirdButtonState);
-    //     digitalWrite(STATUS_PIN, LOW);
-    //     delay(50); // Wait a bit between retransmissions
-    // }
-    // else if (irrecv.decode(&results))
-    // {
-    //     digitalWrite(STATUS_PIN, HIGH);
-    //     storeCode(&results);
-    //     irrecv.resume(); // resume receiver
-    //     digitalWrite(STATUS_PIN, LOW);
-    // }
-    // lastBtnStateOfBtn3 = thirdButtonState;
-//===========================================================
-
-
-
-
+    buttons_list[0].last_button_state = buttons_list[0].button_state;
 
 
 }
